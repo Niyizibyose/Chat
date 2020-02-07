3c04b69d8709fb87960ef08ccf6fea33706e6793
Niyizibyose <jean21101999@gmail.com>
2020-02-07 10:20:43 +0200

    initial

---
+++ signin.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title> Login</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport content="width=device-width" , initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family = Roboto|Courgette|Pacifico:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.boostrapcdn.com/boostrap/3.3.7/css/boostrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.miin.js"></script>
+    <script src="https://maxcdn.boostrapcdn.com/boostrap/3.3.7/js/boostrap.min.js"></script>
+    </head>
+
+    <body>
+
+<div class="sigin-form">
+    <form action="" method="POST">
+        <div class="form-header">
+            <h2>Sign In</h2>
+            <p> Log in to my chat</p>
+        </div>
+
+        <div class="form-group">
+            <label>Email</label>
+            <input type="email" class="form-control" name="email" placeholder="jean21101999@gmail.com" autocomplete="off" required>
+
+        </div>
+
+        <div class="form-group">
+            <label>Password</label>
+            <input type="password" class="form-control" name="pass" placeholder="Password" autocomplete="off" required>
+
+        </div>
+
+        <div class="small"> Forgot passowrd? <a href="forgot_pass.php"> Click here </a></div><br>
+
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary btn-block btn-lg" name="sign_in">Sign in</button> 
+        </div>
+
+        <?php include("signin_user.php"); ?>
+
+       
+    </form>
+</div>
+<div class="text-center small" style="color: #674288;">Don't have an account? <a href="signup.php"> Create one  </a></div>
+
+    </body>
+
+
+</html>
