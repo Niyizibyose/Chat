41a88bd9cf3475cafe239582100a94bd2a810b32
Niyizibyose <jean21101999@gmail.com>
2020-02-11 08:14:42 +0200

    get_users

--- include/get_users_data.php
+++ include/get_users_data.php
@@ -9,6 +9,28 @@ $run_user = mysqli_query ($con , $user);
 while($row_user = mysqli_fetch_array($run_user)){
     $user_id = $row_user['user_id'];
     $user_name = $row_user['user_name'];
+    $user_profile = $row_user['user_profile'];
+    $login = $row_user['log-in'];
+
+    echo"
+        <li>
+            <div class='chat-left-img'>
+            <img src = '$user_profile'>
+
+            </div>
+
+            <div class='chat-left-details'>
+            <p><a href='home.php?user_name = $user_name>$user_name</a></p>
+
+
+        </li>
+    ";
+
+    if ($login == "Online"){
+        echo "<span><i class='fa fa-circle' aria-hidden='true'></i> Online </span>";
+    } else {
+        echo "<span><i class='fa fa-circle-o' aria-hidden='true'></i> Offline </span>";
+    }
 
 }
 
